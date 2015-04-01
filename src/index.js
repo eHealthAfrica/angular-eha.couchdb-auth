603189cc7b7dd663b86cb8d4baef25a7878180d5
Matt Richards <matt@lucidmoon.co.uk>
2015-04-01 15:41:56 +0200

    feat: show-authenticated directive

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
   'use strict';
   var ngModule = angular.module('eha.couchdb-auth', [
     'eha.couchdb-auth.http-interceptor',
-    'eha.couchdb-auth.auth.service'
+    'eha.couchdb-auth.auth.service',
+    'eha.couchdb-auth.show-authenticated.directive'
   ]);
 
   // Check for and export to commonjs environment

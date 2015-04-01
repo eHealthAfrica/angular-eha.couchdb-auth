f97d746ef55508b4d1859f677a604b7410599937
Matt Richards <matt@lucidmoon.co.uk>
2015-04-01 15:41:56 +0200

    feat: show-authenticated directive

---
+++ src/show-authenticated.directive.js
@@ -0,0 +1,30 @@
+angular.module('eha.couchdb-auth.show-authenticated.directive', [])
+  .directive('ehaShowAuthenticated', function(ehaCouchDbAuthService, $animate) {
+    var NG_HIDE_CLASS = 'ng-hide';
+    var NG_HIDE_IN_PROGRESS_CLASS = 'ng-hide-animate';
+    return {
+      restrict: 'A',
+      link: function(scope, element) {
+        // Hide by default
+        element.addClass('ng-hide');
+
+        function checkStatus() {
+          ehaCouchDbAuthService.getCurrentUser()
+            .then(function() {
+              $animate.removeClass(element, NG_HIDE_CLASS, {
+                tempClasses: NG_HIDE_IN_PROGRESS_CLASS
+              });
+            })
+            .catch(function() {
+              $animate.addClass(element, NG_HIDE_CLASS, {
+                tempClasses: NG_HIDE_IN_PROGRESS_CLASS
+              });
+            });
+        }
+
+        checkStatus();
+
+        ehaCouchDbAuthService.on('authenticationStateChange', checkStatus);
+      }
+    };
+  });
